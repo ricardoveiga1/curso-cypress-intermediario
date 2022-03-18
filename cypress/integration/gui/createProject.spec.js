4cfa7ce38389d525a77233ce6f0383c8072bac6c
ricardoveiga1 <ricardoveiga.ti@gmail.com>
2022-03-17 23:28:06 -0300

    Gui tests

---
+++ cypress/integration/gui/createProject.spec.js
@@ -0,0 +1,22 @@
+ ///  <reference types="Cypress" />
+
+ const faker = require('faker')
+
+ describe('Create Project', () => {
+    before(() => cy.login())
+
+    it('successfully', () => {
+
+        const project = {
+            name: `project-${faker.random.uuid()}`,
+            description: faker.random.words(5)
+        }
+
+        cy.gui_createProjects(project)
+
+        cy.url().should('be.equal', `${Cypress.config('baseUrl')}${Cypress.env('user_name')}/${project.name}`)
+        cy.contains(project.name).should('be.visible')
+        cy.contains(project.description).should('be.visible')
+
+    })
+})
